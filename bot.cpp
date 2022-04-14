d97c4aa2fb359067207ac3a049eb6875449442cb
Johan Veenpere <johan.veenpere@gmail.com>
2022-04-14 12:46:56 +0300

    throttle control, based on: 1. angle between bot heading and next checkpoint 2. distance between the bot and next checkpoint (slows down when approaching to not overshoot)

--- bot.cpp
+++ bot.cpp
@@ -2,66 +2,69 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 int prevX, prevY;
 
 bool isNewCheckpoint(int x, int y) {
-    if(prevX == x && prevY == y) return false;
+    if (prevX == x && prevY == y) return false;
     return true;
 }
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
+string outputPosition(int nextCheckpointX, int nextCheckpointY) {
+    return to_string(nextCheckpointX) + " " + to_string(nextCheckpointY);
+}
+
+double speedAngleMultiplier(int angle) {
+    double rad = angle * 3.14149 / 180;
+    return (cos(rad) + 1) / 2;
+}
+
+double aheadOfCheckpointThrottle(int distance) {
+    const int breakingDistance = 10;
+    return min(distance, breakingDistance) / breakingDistance;
+}
+
+double speedMultiplier(int distance, int angle) {
+    return speedAngleMultiplier(angle) * aheadOfCheckpointThrottle(distance);
+}
+
+string outputSpeed(int distance, int angle, int checkpointNr) {
+    string output;
+    if (checkpointNr == 12 && angle == 0) {
+        output += "BOOST";
+    }
+    else {
+        int thrust = 10 + 90 * speedMultiplier(distance, angle);
+        output += to_string(thrust);
+    }
+    return output;
+}
 
 int main()
 {
     int checkpointNr = 0;
     // game loop
-    while (1) {
-        int x;
-        int y;
-        int next_checkpoint_x; // x position of the next check point
-        int next_checkpoint_y; // y position of the next check point
-        int next_checkpoint_dist; // distance to the next checkpoint
-        int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint
-        cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
-        int opponent_x;
-        int opponent_y;
-        cin >> opponent_x >> opponent_y; cin.ignore();
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-        if( isNewCheckpoint(next_checkpoint_x, next_checkpoint_y)){
-            prevX = next_checkpoint_x;
-            prevY = next_checkpoint_y;
-            checkpointNr++;
-        }
+    int x = 0;
+    int y = 0;
+    int next_checkpoint_x = 1000; // x position of the next check point
+    int next_checkpoint_y = 1000; // y position of the next check point
+    int next_checkpoint_dist = 1414; // distance to the next checkpoint
+    int next_checkpoint_angle = 0; // angle between your pod orientation and the direction of the next checkpoint
+    //cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
+    int opponent_x;
+    int opponent_y;
+    //cin >> opponent_x >> opponent_y; cin.ignore();
 
-        cerr << "current checkpoint number: " << checkpointNr << endl;
-
-        string output;
-
-        output+= to_string(next_checkpoint_x) + " ";
-        output+= to_string(next_checkpoint_y) + " ";
-
-        if(checkpointNr == 12 && next_checkpoint_angle == 0) {
-            output+= "BOOST";
-        }
-        else {
-            int thrust;
-            if( next_checkpoint_angle > 90 || next_checkpoint_angle < -90) {
-                thrust = 0;
-            }
-            else {
-                thrust = 100;
-            }
-            output+= to_string(thrust);
-        }
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
-        cout << output << endl;
+    if (isNewCheckpoint(next_checkpoint_x, next_checkpoint_y)) {
+        prevX = next_checkpoint_x;
+        prevY = next_checkpoint_y;
+        checkpointNr++;
     }
+
+    cerr << "current checkpoint number: " << checkpointNr << endl;
+
+    cout << outputPosition(next_checkpoint_x, next_checkpoint_y) << " " << outputSpeed(next_checkpoint_dist, next_checkpoint_angle, checkpointNr) << endl;
 }
